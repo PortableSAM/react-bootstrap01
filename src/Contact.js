98d339753cb2d79e191e68c57777edba64432066
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-21 14:12:30 +0900

    Build a website

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact = () => {
+	return (
+		<div>
+			<h2>Contact Us</h2>
+			<p>거친 것이 예수는 인간은 인간의 있는가? 무엇을 있음으로써 꽃이 스며들어 튼튼하며, 곳으로 새가 힘있다.</p>
+		</div>
+	);
+};
